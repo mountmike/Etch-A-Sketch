65c9664c98d5354038bc1d84da6b6d2a6f869bb2
Michael <tharratt.michael@gmail.com>
2022-09-28 15:34:44 -0700

    Added basic CSS formating

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ function makeRows(rows, cols) {
   gridContainer.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
+    //cell.innerText = (c + 1);
     gridContainer.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
+makeRows(64, 64);
 
+let currentColor = 'black'
 
+const cells = gridContainer.getElementsByClassName('grid-item')
 
+for (const cell of cells) {
+    cell.addEventListener('mouseover', function o () {
+        cell.style.backgroundColor = currentColor;
+    });
+}
